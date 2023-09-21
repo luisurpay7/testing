a96902723cf981d62361a34f5a8018b2a974310c
luisyet <sulcaluis151999@gmail.com>
2023-09-21 11:12:34 -0500

    mocking

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,9 +1,25 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Paco Yunque',
+  },
+];
+
+// Va a suplantar nuestro lib y los métodos necesarios
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe('test for getBooks', () => {
@@ -13,7 +29,16 @@ describe('Test for BooksService', () => {
       const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books.length).toEqual(2);
+      expect(books.length).toEqual(1);
+    });
+
+    test('should return a list of books', async () => {
+      // Arrange
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Paco Yunque');
     });
   });
 });
